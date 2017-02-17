684b2b0175754dbdf1860821c5e1fda5f2d7e80a
zhaohaobo <zhaohaobo@bubi.cn>
2017-02-17 18:33:37 +0800

    docker-plugin

--- source/server-tenant/src/main/java/cn/bubi/baas/tenant/Application.java
+++ source/server-tenant/src/main/java/cn/bubi/baas/tenant/Application.java
@@ -2,11 +2,15 @@ package cn.bubi.baas.tenant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 程序入口
+ * @author hobo
+ *
+ */
 @SpringBootApplication
-public class ServerTenantApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerTenantApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
